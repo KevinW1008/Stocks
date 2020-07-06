78366c8b3530e01da5d1351a41a945b19b598c0b
unknown <kwang10082000@gmail.com>
2020-07-06 10:18:17 -0400

    Stock.py

--- Stock.py
+++ Stock.py
@@ -9,6 +9,14 @@ style.use('ggplot')
 start = dt.datetime(2015, 1, 1)
 end = dt.datetime.now()
 
-df = web.DataReader("TSLA", "stooq", start, end)
+df = web.DataReader("TSLA", "yahoo", start, end)
 
 print(df.head())
+print(df.tail())
+
+df.to_csv("TSLA.csv")
+
+df = pd.read_csv("TSLA.csv", parse_dates = True, index_col = 0 )
+
+df[['High','Low']].plot()
+plt.show()
