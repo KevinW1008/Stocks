396560cca490838ce0bfa0fc7a65c6c606440264
neilshah20 <neilshah20@gmail.com>
2020-07-16 11:14:56 -0400

    Added machine learning component to project model

---
+++ FullStackDevelopment - Alpha/testingLinearRegression/linearregression.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("MSFT.csv", sep=",")
+
+data = data[["Open", "Close"]]
+
+predict = "Close"
+
+X = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test)
+
+print(accuracy)
+
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
