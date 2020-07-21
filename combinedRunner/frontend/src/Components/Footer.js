d0dbf4803901df9639af9d3214f68b4b1bd98547
neilshah20 <neilshah20@gmail.com>
2020-07-21 13:09:29 -0400

    Added more footer functionality and avatars

--- combinedRunner/frontend/src/Components/Footer.js
+++ combinedRunner/frontend/src/Components/Footer.js
@@ -33,59 +33,83 @@ function Footer() {
                       <Image
                         avatar
                         href="https://www.linkedin.com/in/neilshah20"
-                        src="https://media-exp1.licdn.com/dms/image/C4D03AQHXTYNZsmAd9Q/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=1r2lPBR8aJWt7FMZlBZLwCy-1pthFQZjtpIdQBKqtHA"
+                        src="https://neilshah20.github.io/images/portrait-default.jpg"
                       />{" "}
                       Neil Shah
                     </List.Header>
                   </List.Content>
                 </List.Item>
 
-                {/*<List.Item
-                  as="a"
-                  content="Neil Shah"
-                  href="https://www.linkedin.com/in/neilshah20"
-                  target="_blank"
-                />
-                <Image
-                  avatar
-                  src="https://media-exp1.licdn.com/dms/image/C4D03AQHXTYNZsmAd9Q/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=1r2lPBR8aJWt7FMZlBZLwCy-1pthFQZjtpIdQBKqtHA"
-                />
-                <List.Item
-                  as="a"
-                  content="Kevin Wang"
-                  href="https://www.linkedin.com/in/kevinw1008"
-                  target="_blank"
-                />
-                <Image
-                  avatar
-                  src="https://vignette.wikia.nocookie.net/gameofthrones/images/8/89/TywinLannisterCP.png/revision/latest?cb=20140824183623"
-                />
-                <List.Item
-                  as="a"
-                  content="Nick Zhang"
-                  href="https://www.linkedin.com/in/nick-z-421b80140"
-                  target="_blank"
-                /> */}
+                <List.Item>
+                  <List.Content>
+                    <List.Header
+                      href="https://www.linkedin.com/in/kevinw1008"
+                      as="a"
+                    >
+                      <Image
+                        avatar
+                        href="https://www.linkedin.com/in/kevinw1008"
+                        src="https://media-exp1.licdn.com/dms/image/C4E03AQFhUe63nW9sFw/profile-displayphoto-shrink_400_400/0?e=1600905600&v=beta&t=G2fs5DxJIUbzfv0FUDyIuY-wT25CI9DfKu3Z1Of5mJ8"
+                      />{" "}
+                      Kevin Wang
+                    </List.Header>
+                  </List.Content>
+                </List.Item>
+
+                <List.Item>
+                  <List.Content>
+                    <List.Header
+                      href="https://www.linkedin.com/in/nick-z-421b80140"
+                      as="a"
+                    >
+                      <Image
+                        avatar
+                        href="https://www.linkedin.com/in/nick-z-421b80140"
+                        src="https://media-exp1.licdn.com/dms/image/C5603AQFI-6uW2jvxKw/profile-displayphoto-shrink_400_400/0?e=1600905600&v=beta&t=KoQSGcGIhKVgYYTEt9GgE6M5c159QfZNiMjQ_ocm-Vk"
+                      />{" "}
+                      Nick Zhang
+                    </List.Header>
+                  </List.Content>
+                </List.Item>
               </List>
             </Grid.Column>
 
             <Grid.Column width={3}>
-              <Header inverted as="h4" content="GitHub Repo" />
+              <Header inverted as="h4" content="GitHub Info" />
               <List link inverted>
                 <List.Item
                   as="a"
-                  content="Github Page"
+                  content="Project Repository"
                   href="https://github.com/KevinW1008/Stocks.git"
+                  target="_blank"
+                />
+                <List.Item
+                  as="a"
+                  content="Kevin Wang"
+                  href="https://github.com/KevinW1008/"
+                  target="_blank"
+                />
+                <List.Item
+                  as="a"
+                  content="Neil Shah"
+                  href="https://github.com/neilshah20/"
+                  target="_blank"
+                />
+                <List.Item
+                  as="a"
+                  content="Nick Zhang"
+                  href="https://github.com/jazhang1999/"
                   target="_blank"
                 />
               </List>
             </Grid.Column>
-            <Grid.Column width={7}>
+            {/*<Grid.Column width={7}>
               <Header as="h4" inverted>
                 Footer Header
               </Header>
               <p>Any man who must say I am the king is no true king</p>
             </Grid.Column>
+  */}
           </Grid.Row>
         </Grid>
       </Container>
